731a2b445e9c7b49d0ae59193f7b812002d1ada5
prabhat.k <prabhat.k@eshopbox.com>
2023-07-20 02:19:29 +0530

    application properties added

--- src/main/java/com/test/demo/DemoApplication.java
+++ src/main/java/com/test/demo/DemoApplication.java
@@ -2,8 +2,18 @@ package com.test.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.test.demo.dao"})
+@EntityScan(basePackages = {"com.test.demo.model"} )
+@ComponentScan(
+		basePackages = {
+				"com.test.demo.*"
+		}
+)
 public class DemoApplication {
 
 	public static void main(String[] args) {
